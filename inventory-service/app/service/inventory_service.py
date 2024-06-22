e9db01ad90845971b08eb135f50c14b44a9e6fe3
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-06-22 21:50:36 +0500

    code commited successfully

---
+++ inventory-service/app/service/inventory_service.py
@@ -0,0 +1,49 @@
+from ..model.models import Product, ProductSize, ProductItem, ProductItemFormModel, SizeModel, Stock
+from fastapi import APIRouter, Depends, UploadFile, File, Form, HTTPException
+from ..utils.admin_verify import get_current_active_admin_user
+from typing import Annotated, Optional, List
+from ..model.admin import Admin
+from ..core.db import DB_SESSION
+from sqlmodel import select
+
+# Product Item
+async def create_product_item(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
+                    session: DB_SESSION,
+                    product_item_detail: ProductItemFormModel,
+                    image: UploadFile = File(...)
+                    ):
+    return {"message" : "create product items"}
+
+async def get_product_item(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
+                    session: DB_SESSION,
+                    product_id: int):
+    return {"message" : "get product items"}
+
+async def delete_product_item(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
+                    session: DB_SESSION,
+                    product_item_id: int):
+    return {"message" : "delete product items"}
+
+# Product Sizes
+async def product_size(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
+                        session: DB_SESSION,
+                        product_size_detail: SizeModel,
+                        product_item_id: int
+                        ):
+    return {"message" : "create product size"}
+
+async def get_product_size(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
+                        session: DB_SESSION,
+                        product_item_id: int):
+    return {"message" : "get product size"}
+
+async def delete_product_size(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
+                        session: DB_SESSION,
+                        product_size_id: int):
+    return {"message" : "delete product size"}
+
+async def update_product_size(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
+                        session: DB_SESSION,
+                        product_size_detail: SizeModel,
+                        product_size_id: int):
+    return {"message" : "update product size"}
--- inventory-service/app/web/routes.py
+++ inventory-service/app/web/routes.py
@@ -16,21 +16,21 @@ async def create_product_items(current_admin: Annotated[Admin, Depends(get_curre
                     product_item_detail: ProductItemFormModel,
                     image: UploadFile = File(...)
                     ):
-    # product_item = async create_product_item(current_admin, session, product_item_detail, image)
+    # product_item = await create_product_item(current_admin, session, product_item_detail, image)
     return {"message" : "Item of Product Create Successfully!"}
 
 @router.get("/product_item")
 async def get_product_items(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
                     session: DB_SESSION,
                     product_id: int):
-    # product_items = async get_product_item(current_admin, session, product_id)
+    # product_items = await get_product_item(current_admin, session, product_id)
     return {"message" : "Item of Product Get Successfully!"}
 
 @router.delete("/product_item")
 async def delete_product_items(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
                     session: DB_SESSION,
                     product_item_id: int):
-    #product_item = async delete_product_item(current_admin, session, product_item_id)
+    #product_item = await delete_product_item(current_admin, session, product_item_id)
     return {"message" : "Item of Product Delete Successfully!"}
 
 @router.post("/product_Size")
@@ -39,21 +39,21 @@ async def product_sizes(current_admin: Annotated[Admin, Depends(get_current_acti
                         product_size_detail: SizeModel,
                         product_item_id: int
                         ):
-    # product_size = async create_product_size(current_admin, session, product_size_detail, product_item_id)
+    # product_size = await create_product_size(current_admin, session, product_size_detail, product_item_id)
     return {"message" : "Size of Product Create Successfully!"}
 
 @router.get("/product_Size")
 async def get_product_sizes(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
                         session: DB_SESSION,
                         product_item_id: int):
-    # product_sizes = async get_product_size(current_admin, session, product_item_id)
+    # product_sizes = await get_product_size(current_admin, session, product_item_id)
     return {"message" : "Size of Product Get Successfully!"}
 
 @router.delete("/product_Size")
 async def delete_product_sizes(current_admin: Annotated[Admin, Depends(get_current_active_admin_user)],
                         session: DB_SESSION,
                         product_size_id: int):
-    #product_size = async delete_product_size(current_admin, session, product_size_id)
+    #product_size = await delete_product_size(current_admin, session, product_size_id)
     return {"message" : "Size of Product Delete Successfully!"}
 
 @router.put("/product_Size")
@@ -61,5 +61,5 @@ async def update_product_sizes(current_admin: Annotated[Admin, Depends(get_curre
                         session: DB_SESSION,
                         product_size_detail: SizeModel,
                         product_size_id: int):
-    #product_size = async update_product_size(current_admin, session, product_size_detail, product_size_id)
+    #product_size = await update_product_size(current_admin, session, product_size_detail, product_size_id)
     return {"message" : "Size of Product Update Successfully!"}
