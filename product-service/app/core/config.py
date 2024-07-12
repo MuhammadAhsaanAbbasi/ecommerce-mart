d4aba6376df01834b42a9e22a44061dcaa804a8b
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-07-12 16:45:16 +0500

    category service update

--- product-service/app/core/config.py
+++ product-service/app/core/config.py
@@ -1,13 +1,29 @@
-import logging
+from typing import List
+import boto3 # type: ignore
+from botocore.exceptions import NoCredentialsError # type: ignore
+from fastapi import HTTPException, UploadFile
+from app.setting import BUCKET_NAME, AWS_REGION, AWS_ACCESS_KEY, AWS_SECRET_KEY
 
 
-def logger_config(module):
-    """
-    Logger function.
-    - can Extend Python loggin module and set a custom config.
-    """
-    logging.basicConfig(level=logging.INFO)
+def upload_files_in_s3(files: List[UploadFile], product_id: int):
+    s3_client = boto3.client("s3",
+                            aws_access_key_id=AWS_ACCESS_KEY,
+                            aws_secret_access_key=AWS_SECRET_KEY
+                            )
+    image_urls: List[str] = []
+    for file in files:
+        try:
+            object_name = f"products/{product_id}/{file.filename}"
+            print(f"Object Name : {object_name}")
+            s3_client.upload_fileobj(
+                file.file,
+                BUCKET_NAME,
+                object_name
+            )
+            image_url = f"https://{BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{
+                object_name}"
+            image_urls.append(image_url)
+        except NoCredentialsError as nce:
+            raise HTTPException(status_code=500, detail=str(nce))
 
-    logger = logging.getLogger(module)
-
-    return logger
+    return image_urls
--- product-service/app/setting.py
+++ product-service/app/setting.py
@@ -11,8 +11,10 @@ TEST_DATABASE_URL = config("TEST_DATABASE_URL", cast=Secret)
 SECRET_KEY = config("SECRET_KEY", cast=str)
 ALGORITHM = config("ALGORITHM", cast=str)
 
-CLOUDINARY_CLOUD = config("CLOUDINARY_CLOUD", cast=str)
-CLOUDINARY_API_KEY = config("CLOUDINARY_API_KEY", cast=str)
-CLOUDINARY_API_SECRET = config("CLOUDINARY_API_SECRET", cast=str)
+BUCKET_NAME = config("BUCKET_NAME", cast=str)
+AWS_REGION = config("AWS_REGION", cast=str)
+AWS_ACCESS_KEY = config("AWS_ACCESS_KEY", cast=str)
+AWS_SECRET_KEY = config("AWS_SECRET_KEY", cast=str)
+
 
 PRODUCT_TOPIC = config("PRODUCT_TOPIC", cast=str)
